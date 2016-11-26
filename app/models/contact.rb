b39e31dc31cdb4c7e2656acc704e661982eb367a
Grigory Makarenko <grigory.makarenko@gmail.com>
2016-11-26 03:40:55 +0000

    Fixing again -_-

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
-=======
-class Contact < ActiveRecord::Base
-  
->>>>>>> 470a74a1190a8e0b536e6c8eb09d0447897c4595
 end
